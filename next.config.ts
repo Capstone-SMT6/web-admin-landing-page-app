489d7ff360980d9f82d5e337cb3ba463d789b857
Kotsuke <Subandrio140705@gmail.com>
2026-07-02 08:46:30 +0700

    Configure secure backend proxy using Next.js rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/backend_api/:path*',
+        destination: `${process.env.SECRET_BACKEND_URL}/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
